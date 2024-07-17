8d9be99a2b48896276c3c2ff82a0c1178f818f85
Isaac Detert <detert7392@students.cptc.edu>
2024-07-16 18:48:55 -0700

    Start of Program.cs

--- DiceRoller - Isaac Detert/Program.cs
+++ DiceRoller - Isaac Detert/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using DiceRoller___Isaac_Detert;
+
+Die myDie = new Die(); // Get a random number when die is created
+// Setting face value should not be allowed outside of class
+byte currentValue = myDie.FaceValue;
+myDie.Roll();
+
+Console.ReadKey();
