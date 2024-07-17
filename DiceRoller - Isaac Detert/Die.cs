093ade704acea200b974be60ef9c2f908eb66eaa
Isaac Detert <detert7392@students.cptc.edu>
2024-07-16 19:28:25 -0700

    Refactored roll method to take into account if the die is held or not.

--- DiceRoller - Isaac Detert/Die.cs
+++ DiceRoller - Isaac Detert/Die.cs
@@ -32,18 +32,21 @@ namespace DiceRoller___Isaac_Detert
 
 		/// <summary>
 		/// Rolls the die and sets the <see cref="FaceValue"/>
-		/// to the new number. Returns the new number
+		/// to the new number if the die is not
+		/// currently held. Returns the <see cref="FaceValue"/>
 		/// </summary>
-		/// <returns>Returns the new random number</returns>
+		/// <returns>Returns the <see cref="FaceValue"/></returns>
 		public byte Roll()
 		{
-			// Generate random number
-			Random random = new Random();
-			byte newValue = (byte)random.Next(1, 7);
+			if (!IsHeld)
+			{
+				// Generate random number
+				Random random = new Random();
+				byte newValue = (byte)random.Next(1, 7);
 
-			// Set to face value
-			FaceValue = newValue;
-
+				// Set to face value
+				FaceValue = newValue;
+			}
 			// Return new number
 			return FaceValue;
 		}
